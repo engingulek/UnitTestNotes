529b6140f0b19d22ed9f45e1fa3fd93739f9d487
Engin Gülek <engingulek0@gmail.com>
2023-05-29 14:43:44 +0300

    UITest

--- UnitTestExample/UnitTestExampleUITests/UnitTestExampleUITests.swift
+++ UnitTestExample/UnitTestExampleUITests/UnitTestExampleUITests.swift
@@ -8,26 +8,39 @@
 import XCTest
 
 final class UnitTestExampleUITests: XCTestCase {
-
+    var app : XCUIApplication!
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
+        try super.setUpWithError()
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
+        app = XCUIApplication()
         app.launch()
+        
+        
+    }
 
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func testGameStyleSwitch() throws {
+        let app = XCUIApplication()
+        let slideButton = app.segmentedControls.buttons["Slide"]
+        let typeButton = app.segmentedControls.buttons["Type"]
+        let slideLabel = app.staticTexts["Get as close as you can to: "]
+        let typeLabel = app.staticTexts["Guess where the slider is: "]
+        
+        if slideButton.isSelected {
+          XCTAssertTrue(slideLabel.exists)
+          XCTAssertFalse(typeLabel.exists)
+
+          typeButton.tap()
+          XCTAssertTrue(typeLabel.exists)
+          XCTAssertFalse(slideLabel.exists)
+        } else if typeButton.isSelected {
+          XCTAssertTrue(typeLabel.exists)
+          XCTAssertFalse(slideLabel.exists)
+
+          slideButton.tap()
+          XCTAssertTrue(slideLabel.exists)
+          XCTAssertFalse(typeLabel.exists)
+        }
     }
 
     func testLaunchPerformance() throws {
